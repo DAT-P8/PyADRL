cdb960b29f116b021ea12eff89f756a19bb5855b
Mads Byriel <madshbyriel@gmail.com>
2026-03-17 11:11:38 +0100

    Setup build step with 3D protos

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ cd "$(dirname $0)"
 
 python -m grpc_tools.protoc -Iprotos --python_out=PyADRL --pyi_out=PyADRL --grpc_python_out=PyADRL protos/grid_world.proto
 
+python -m grpc_tools.protoc -Iprotos --python_out=PyADRL --pyi_out=PyADRL --grpc_python_out=PyADRL protos/TDF.proto
+
 sed -i 's/^import grid_world_pb2/from . import grid_world_pb2/' PyADRL/grid_world_pb2_grpc.py
+
+sed -i 's/^import TDF_pb2/from . import TDF_pb2/' PyADRL/TDF_pb2_grpc.py
