057e227df6a1e78e30ebfd83fdb1bc2953889ab2
Mads Byriel <madshbyriel@gmail.com>
2026-03-17 14:55:20 +0100

    Absolutely fucking genius groups.google !

--- build.sh
+++ build.sh
@@ -5,9 +5,17 @@ set -e
 cd "$(dirname $0)"
 
 python -m grpc_tools.protoc -Iprotos --python_out=PyADRL --pyi_out=PyADRL --grpc_python_out=PyADRL protos/grid_world.proto
-
 python -m grpc_tools.protoc -Iprotos --python_out=PyADRL --pyi_out=PyADRL --grpc_python_out=PyADRL protos/TDF.proto
 
 sed -i 's/^import grid_world_pb2/from . import grid_world_pb2/' PyADRL/grid_world_pb2_grpc.py
-
 sed -i 's/^import TDF_pb2/from . import TDF_pb2/' PyADRL/TDF_pb2_grpc.py
+
+echo "import grpc.experimental" > delme.txt
+cat ./PyADRL/grid_world_pb2_grpc.py >> delme.txt
+cat delme.txt > ./PyADRL/grid_world_pb2_grpc.py
+
+echo "import grpc.experimental" > delme.txt
+cat ./PyADRL/TDF_pb2_grpc.py >> delme.txt
+cat delme.txt > ./PyADRL/TDF_pb2_grpc.py
+
+rm delme.txt
