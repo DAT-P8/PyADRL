2fc11ac8bab0f90ece6587a62f3da2e6ee301a73
SandraRosenbeck <112960003+SandraRosenbeck@users.noreply.github.com>
2026-03-25 11:43:39 +0100

    checkpoint folder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import PyADRL.examples.gridworld
+import os
+from os.path import exists
 
 GRIDWORLD = "gridworld"
 
@@ -47,6 +49,9 @@ def parse_args() -> argparse.Namespace:
 
 
 def main():
+    if not exists("checkpoints"):
+        os.makedirs("checkpoints")
+
     args = parse_args()
 
     if args.train:
