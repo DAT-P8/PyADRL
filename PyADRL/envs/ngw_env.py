b4dadf69be02dd5403b1040e1a731b34fa4dbc3a
Mads Byriel <madshbyriel@gmail.com>
2026-04-23 11:29:37 +0200

    Remove velocity from newrequest

--- PyADRL/envs/ngw_env.py
+++ PyADRL/envs/ngw_env.py
@@ -111,8 +111,7 @@ class NGWEnvironment(ParallelEnv):
                 NewRequest(
                     map=self.map_config.get_map_spec(),
                     evader_count=self.n_evaders,
-                    pursuer_count=self.n_pursuers,
-                    drone_velocity=self.drone_velocity,
+                    pursuer_count=self.n_pursuers
                 )
             )
             self.id = state.sim_id
